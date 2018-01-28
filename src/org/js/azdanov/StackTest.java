3d6152fee029e80602a10887dbfba6c60cccb5a2
Anton Zdanov <akumael@gmail.com>
2018-01-28 18:00:15 +0200

    add push

--- src/org/js/azdanov/StackTest.java
+++ src/org/js/azdanov/StackTest.java
@@ -1,15 +1,25 @@
 package org.js.azdanov;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class StackTest {
 
   @Test
-  void newlyCreatedStackIsEmpty() throws Exception {
+  void newlyCreatedStackIsEmpty() {
     Stack stack = new Stack();
     assertTrue(stack.isEmpty());
     assertEquals(0, stack.getSize());
+  }
+
+  @Test
+  void onPushStackSizeIsOne() {
+    Stack stack = new Stack();
+    stack.push(1);
+    assertEquals(1, stack.getSize());
+    assertFalse(stack.isEmpty());
   }
 }
