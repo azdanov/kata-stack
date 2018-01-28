2e31d53305115a3bc2070e04477ad9a4139e6504
Anton Zdanov <akumael@gmail.com>
2018-01-28 18:07:04 +0200

    refactor stack creation into before block

--- src/org/js/azdanov/StackTest.java
+++ src/org/js/azdanov/StackTest.java
@@ -4,20 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StackTest {
 
+  private Stack stack;
+
+  @BeforeEach
+  public void setUp() {
+    stack = new Stack();
+  }
+
   @Test
   void newlyCreatedStackIsEmpty() {
-    Stack stack = new Stack();
     assertTrue(stack.isEmpty());
     assertEquals(0, stack.getSize());
   }
 
   @Test
   void onPushStackSizeIsOne() {
-    Stack stack = new Stack();
     stack.push(1);
     assertEquals(1, stack.getSize());
     assertFalse(stack.isEmpty());
